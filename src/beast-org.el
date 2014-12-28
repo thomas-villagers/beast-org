bb547fd6bf6395b927a1102643a7cee5aec902c6
thomas <thomas@tower>
2014-12-28 15:46:20 +0100

    use alist instead of hash table makes code cleaner

--- src/beast-org.el
+++ src/beast-org.el
@@ -2,6 +2,10 @@
 
 (defun get-date () (cdr (assoc "SCHEDULED" (org-entry-properties))))
 
+(defun get-time () (cdr (assoc "Time" (org-entry-properties))))
+
+(defun alist-get-value (key alist) (car (cdr (assoc key alist))))
+
 (defun line-func (c e d)
   (if (eq e nil) (reverse d)
     (line-func (+ c 1) (cdr e) (cons (string-to-number (org-table-get-field c)) d))))
@@ -10,29 +14,24 @@
   (search-forward excercise)
   (let ((column (org-table-current-column)))
     (push
-     (line-func (+ column 1) (gethash set sethash) (list (get-date)))
+     (line-func (+ column 1) (alist-get-value set setalist) (list (get-date)))
      mylist)))
 
 (defun make-table (workout excercise set)
-  (let ((excercise excercise)
-        (sethash (make-hash-table :test 'equal)))
-    (puthash "singleset" '("15 Reps" "12 Reps" "8 Reps") sethash)
-    (puthash "singleset2" '("15 Reps" "12 Reps" "8 Reps" "8 Reps") sethash)
-    (puthash "singleset12" '("12 Reps" "10 Reps" "8 Reps") sethash)
-    (puthash "singleset10" '("10 Reps" "10 Reps" "10 Reps") sethash)
-    (puthash "singleset50" '("50 Reps" "50 Reps") sethash)
-    (puthash "singleset15" '("15 Reps" "12 Reps") sethash)
-    (puthash "singleset30s" '("30 Sec." "30 Sec.") sethash)
-    (puthash "singleset30" '("30 Reps") sethash)
-    (puthash "forceset" '("5 Reps" "5 Reps" "5 Reps" "5 Reps" "5 Reps") sethash)
-    (puthash "progressiveset" '("15 Reps" "12 Reps" "8 Reps" "8 Reps" "12 Reps" "15 Reps") sethash)
-    (setq mylist (list (cons "Date" (gethash set sethash))))
+  (let ((setalist '(("singleset"    ("15 Reps" "12 Reps" "8 Reps")) 
+		    ("singleset2"   ("15 Reps" "12 Reps" "8 Reps" "8 Reps"))
+		    ("singleset12"  ("12 Reps" "10 Reps" "8 Reps"))
+		    ("singleset10"  ("10 Reps" "10 Reps" "10 Reps"))
+		    ("singleset50"  ("50 Reps" "50 Reps"))
+		    ("singleset15"  ("15 Reps" "12 Reps"))
+		    ("singleset30s" ("30 Sec." "30 Sec."))		   
+		    ("singleset30"  ("30 Reps"))
+		    ("forceset"     ("5 Reps" "5 Reps" "5 Reps" "5 Reps" "5 Reps"))
+		    ("progressiveset" ("15 Reps" "12 Reps" "8 Reps" "8 Reps" "12 Reps" "15 Reps")))))
+    (setq mylist (list (cons "Date" (alist-get-value set setalist))))
     (push 'hline mylist)
     (org-map-entries 'table-func workout (list beast-org-file))
     (reverse mylist)))
-
-(defun get-time ()
-  (cdr (assoc "Time" (org-entry-properties))))
 
 (defun process-run-entry ()
   (push (list (get-date) (get-time)) the-list))
